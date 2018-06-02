9ea4c81f3654fed1900177a426a0c7572623c515
Aleksandr Siryi <theobroma222@gmail.com>
2018-06-02 17:35:53 +0300

    finished

---
+++ src/color.ts
@@ -0,0 +1,14 @@
+import { INamed } from './common';
+
+export class Color implements INamed {
+  constructor(public name: string) {}
+}
+
+export class Colors extends Array<Color> {
+  constructor() {
+    super();
+    ['Red', 'Orange', 'Yellow', 'Blue', 'Indigo', 'Violet'].forEach(color => {
+      this.push(new Color(color));
+    });
+  }
+}
